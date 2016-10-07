83f0fc43175b133a14ed2e3407f149a3bdf0425d
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-07 14:17:27 +0000

    AMI lookup

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -3,3 +3,11 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
