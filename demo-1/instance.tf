4e078036afb57364b248b79baafecec9f36a5ac6
Edward Viaene <ward@in4it.io>
2016-10-07 16:13:30 +0200

    demo-1

---
+++ demo-1/instance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+}
---
+++ demo-1/vars.tf
@@ -0,0 +1,5 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
