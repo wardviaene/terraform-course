a85c62ce7dd65335703b6b528ac6ebc116f71cee
Edward Viaene <ward.viaene@gmail.com>
2016-10-10 14:52:50 +0000

    demo-4

---
+++ demo-4/instance.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "example" {
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
+}
+output "ip" {
+    value = "${aws_instance.example.public_ip}"
+}
---
+++ demo-4/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
