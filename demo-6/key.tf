6a43f9a1ba50d50938b49f5b26e240049823c04a
Edward Viaene <ward.viaene@gmail.com>
2016-10-11 17:03:29 +0000

    demo 6

---
+++ demo-6/key.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "mykey" {
+  key_name = "mykey"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
---
+++ demo-6/modules.tf
@@ -0,0 +1,8 @@
+module "consul" {
+  source = "github.com/wardviaene/terraform-consul-module.git"
+  key_name = "${aws_key_pair.mykey.key_name}"
+  key_path = "${var.PATH_TO_PRIVATE_KEY}"
+}
+output "consul-output" {
+  value = "${module.consul.server_address}"
+}
