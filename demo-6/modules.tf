6dd458a2ab83dd45002ad517f735291c4ed802d0
Edward Viaene <ward.viaene@gmail.com>
2016-10-11 17:59:18 +0000

    added region

--- demo-6/modules.tf
+++ demo-6/modules.tf
@@ -2,6 +2,7 @@ module "consul" {
   source = "github.com/wardviaene/terraform-consul-module.git"
   key_name = "${aws_key_pair.mykey.key_name}"
   key_path = "${var.PATH_TO_PRIVATE_KEY}"
+  region = "${var.AWS_REGION}"
 }
 output "consul-output" {
   value = "${module.consul.server_address}"
