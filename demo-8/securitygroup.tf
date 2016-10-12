a21b60fe40c2cdc1c41bb95d7f13146941c6801d
Edward Viaene <ward.viaene@gmail.com>
2016-10-12 16:17:09 +0000

    protocol tcp instead of -1

--- demo-8/securitygroup.tf
+++ demo-8/securitygroup.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "allow-ssh" {
   ingress {
       from_port = 22
       to_port = 22
-      protocol = "-1"
+      protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   } 
 tags {
