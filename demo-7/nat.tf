9d3c2936ea969fe191ae6ad92f4b370c41380000
Edward Viaene <ward.viaene@gmail.com>
2016-10-12 12:26:00 +0000

    demo-7

---
+++ demo-7/nat.tf
@@ -0,0 +1,35 @@
+# nat gw
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id = "${aws_subnet.main-public-1.id}"
+  depends_on = ["aws_internet_gateway.main-gw"]
+}
+
+# VPC setup for NAT
+resource "aws_route_table" "main-private" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
+    }
+
+    tags {
+        Name = "main-private-1"
+    }
+}
+# route associations private
+resource "aws_route_table_association" "main-private-1-a" {
+    subnet_id = "${aws_subnet.main-private-1.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
+resource "aws_route_table_association" "main-private-2-a" {
+    subnet_id = "${aws_subnet.main-private-2.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
+resource "aws_route_table_association" "main-private-3-a" {
+    subnet_id = "${aws_subnet.main-private-3.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
