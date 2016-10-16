f07125fdda38cd497ffb927fa8410b8ce8797e42
Edward Viaene <ward.viaene@gmail.com>
2016-10-16 16:44:10 +0000

    demo-12

---
+++ demo-12/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+variable "RDS_PASSWORD" { }
