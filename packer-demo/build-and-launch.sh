fa907a3219affe6e7c83f6122f814c07438aed1f
Edward Viaene <ward.viaene@gmail.com>
2016-10-17 13:34:45 +0000

    filter

--- packer-demo/build-and-launch.sh
+++ packer-demo/build-and-launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-AMI_ID=`packer build -machine-readable packer-example.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+ARTIFACT=`packer build -machine-readable packer-example.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+AMI_ID=`echo $ARTIFACT |cut -d ':' -f2`
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
 terraform apply
