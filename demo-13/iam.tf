38c7371debf302a1145384cca8db7f9262854e2e
Edward Viaene <ward.viaene@gmail.com>
2016-10-17 08:20:33 +0000

    demo-13

---
+++ demo-13/iam.tf
@@ -0,0 +1,24 @@
+# group definition
+resource "aws_iam_group" "administrators" {
+    name = "administrators"
+}
+resource "aws_iam_policy_attachment" "administrators-attach" {
+    name = "administrators-attach"
+    groups = ["${aws_iam_group.administrators.name}"]
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+# user
+resource "aws_iam_user" "admin1" {
+    name = "admin1"
+}
+resource "aws_iam_user" "admin2" {
+    name = "admin2"
+}
+resource "aws_iam_group_membership" "administrators-users" {
+    name = "administrators-users"
+    users = [
+        "${aws_iam_user.admin1.name}",
+        "${aws_iam_user.admin2.name}",
+    ]
+    group = "${aws_iam_group.administrators.name}"
+}
