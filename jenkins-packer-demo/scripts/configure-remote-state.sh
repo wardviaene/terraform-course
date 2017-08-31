eaa565c398ca3731c5ddfb68dc178d9698cd9fc8
Edward Viaene <ward.viaene@gmail.com>
2017-08-31 18:26:01 +0200

    removed legacy code

--- jenkins-packer-demo/scripts/configure-remote-state.sh
+++ jenkins-packer-demo/scripts/configure-remote-state.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # not necessary anymore in newer terraform versions, you can use a backend.tf file
-#terraform remote config -backend=s3 -backend-config="bucket=terraform-state-a2b621f" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=eu-west-1"
+if [ "`terraform --version |head -n1`" == "Terraform v0.7.7" ] ; then
+  terraform remote config -backend=s3 -backend-config="bucket=terraform-state-a2b621f" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=eu-west-1"
+fi
