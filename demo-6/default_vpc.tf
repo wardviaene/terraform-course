9fe3f8aa1fcf017e3aee687a81d46b22bf914b1d
Edward Viaene <ward.viaene@gmail.com>
2017-10-31 20:44:52 +0000

    updated to latest version

---
+++ demo-6/default_vpc.tf
@@ -0,0 +1,33 @@
+# these resources are added to provide the vpc_id and subnets to the consul module
+# aws_default_* are resources that terraform doesn't creat, rather adopt
+# See https://www.terraform.io/docs/providers/aws/r/default_vpc.html for more info
+
+# default VPC
+resource "aws_default_vpc" "default" {
+    tags {
+        Name = "Default VPC"
+    }
+}
+
+# default subnets
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "${var.AWS_REGION}a"
+
+    tags {
+        Name = "Default subnet for ${var.AWS_REGION}a"
+    }
+}
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "${var.AWS_REGION}b"
+
+    tags {
+        Name = "Default subnet for ${var.AWS_REGION}b"
+    }
+}
+resource "aws_default_subnet" "default_az3" {
+  availability_zone = "${var.AWS_REGION}c"
+
+    tags {
+        Name = "Default subnet for ${var.AWS_REGION}c"
+    }
+}
--- demo-6/modules.tf
+++ demo-6/modules.tf
@@ -3,6 +3,12 @@ module "consul" {
   key_name = "${aws_key_pair.mykey.key_name}"
   key_path = "${var.PATH_TO_PRIVATE_KEY}"
   region = "${var.AWS_REGION}"
+  vpc_id = "${aws_default_vpc.default.id}"
+  subnets {
+    "0" = "${aws_default_subnet.default_az1.id}"
+    "1" = "${aws_default_subnet.default_az2.id}"
+    "2" = "${aws_default_subnet.default_az3.id}"
+  }
 }
 output "consul-output" {
   value = "${module.consul.server_address}"
