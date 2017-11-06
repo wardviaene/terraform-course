21c7878facd3ee777a5ab4122c5f84cfc8f4e15e
Edward Viaene <ward.viaene@gmail.com>
2017-11-06 08:47:24 +0000

    add code to make sure instance is ready

--- demo-2/script.sh
+++ demo-2/script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
+
+# install nginx
 apt-get update
 apt-get -y install nginx
+
+# make sure nginx is started
+service nginx start
