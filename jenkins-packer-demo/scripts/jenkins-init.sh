9ab6b5ba46c3a1129d73597dc543818047e63460
Edward Viaene <ward.viaene@gmail.com>
2017-11-29 09:00:10 +0000

    jenkins init fixes

--- jenkins-packer-demo/scripts/jenkins-init.sh
+++ jenkins-packer-demo/scripts/jenkins-init.sh
@@ -5,10 +5,20 @@ vgchange -ay
 
 DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
 if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
-	pvcreate ${DEVICE}
-	vgcreate data ${DEVICE}
-	lvcreate --name volume1 -l 100%FREE data
-	mkfs.ext4 /dev/data/volume1
+  # wait for the device to be attached
+  DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
+  DEVICEEXISTS=''
+  while [[ -z $DEVICEEXISTS ]]; do
+    echo "checking $DEVICENAME"
+    DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
+    if [[ $DEVICEEXISTS != "1" ]]; then
+      sleep 15
+    fi
+  done
+  pvcreate ${DEVICE}
+  vgcreate data ${DEVICE}
+  lvcreate --name volume1 -l 100%FREE data
+  mkfs.ext4 /dev/data/volume1
 fi
 mkdir -p /var/lib/jenkins
 echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' >> /etc/fstab
