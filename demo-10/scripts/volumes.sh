491fe03355a18da7041e16ecb953bfbe9efe52f1
Edward Viaene <ward.viaene@gmail.com>
2017-11-29 07:57:14 +0000

    volume.sh script fixes

--- demo-10/scripts/volumes.sh
+++ demo-10/scripts/volumes.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+set -ex 
+
 vgchange -ay
 
 DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
 if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
-	pvcreate ${DEVICE}
-	vgcreate data ${DEVICE}
-	lvcreate --name volume1 -l 100%FREE data
-	mkfs.ext4 /dev/data/volume1
+  # wait for the device to be attached
+  DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
+  DEVICEEXISTS=''
+  while [[ -z $DEVICEEXISTS ]]; do
+    echo "checking $DEVICENAME"
+    DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
+    if [[ $DEVICEEXISTS != "1" ]]; then
+      sleep 15
+    fi
+  done
+  pvcreate ${DEVICE}
+  vgcreate data ${DEVICE}
+  lvcreate --name volume1 -l 100%FREE data
+  mkfs.ext4 /dev/data/volume1
 fi
 mkdir -p /data
 echo '/dev/data/volume1 /data ext4 defaults 0 0' >> /etc/fstab
 mount /data
+
+# install docker
+curl https://get.docker.com | bash
