6104cdf137b91dac850cc412ad537c08e7a34aa4
Edward Viaene <ward.viaene@gmail.com>
2017-12-12 22:15:18 +0100

    fixing egrep

--- packer-demo/build-and-launch.sh
+++ packer-demo/build-and-launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 packer build -machine-readable packer-example.json | tee build.log
-AMI_ID=`egrep -m1 -oe 'ami-.{8}' build.log`
+AMI_ID=`egrep -oe 'ami-.{8}' build.log |tail -n1`
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
 terraform apply
