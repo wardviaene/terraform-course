e4ae2ee0485eb2940aae0af7f47e1e30b8d534da
Edward Viaene <ward.viaene@gmail.com>
2018-06-16 06:44:22 +0000

    added terraform init

--- packer-demo/build-and-launch.sh
+++ packer-demo/build-and-launch.sh
@@ -2,4 +2,5 @@
 packer build -machine-readable packer-example.json | tee build.log
 AMI_ID=`egrep -oe 'ami-.{8}' build.log |tail -n1`
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
+terraform init
 terraform apply
