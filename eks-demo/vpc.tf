d30c8966e84fc022d90a5dbe6f6db5ad24aa1f1a
Edward Viaene <ward@in4it.io>
2019-06-14 18:21:48 +0200

    fix vpc module

--- eks-demo/vpc.tf
+++ eks-demo/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=terraform-012"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.6.0"
 
   name = "vpc-module-demo"
   cidr = "10.0.0.0/16"
