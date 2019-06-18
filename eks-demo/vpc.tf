ab860df912db2db87b4770fb750cc331ffc56186
Edward Viaene <ward@in4it.io>
2019-06-18 15:11:00 +0200

    fix syntax

--- eks-demo/vpc.tf
+++ eks-demo/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "vpc-module-demo"
   cidr = "10.0.0.0/16"
 
-  azs             = [slice(data.aws_availability_zones.available.names, 0, 3)]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
