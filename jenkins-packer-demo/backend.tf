9586c7f64c6f02abf2718972f113ab6f51f4787a
Edward Viaene <ward@in4it.io>
2020-03-17 21:22:53 +0100

    comment backend.tf

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-xx70dpnh"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-xx70dpnh"
+#    key    = "terraform.tfstate"
+#    region = "eu-west-1"
+#  }
+#}
