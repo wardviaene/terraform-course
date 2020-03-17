c544c021b68f0092f139be9399f2f7b103ef426f
Edward Viaene <ward@in4it.io>
2020-03-17 14:10:07 +0100

    added region

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -2,7 +2,7 @@
 set -ex
 AWS_REGION="eu-west-1"
 cd jenkins-packer-demo
-S3_BUCKET=`aws s3 ls |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
