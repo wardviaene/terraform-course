0a6bddd97a8ae164f6b27116c0c713cd8ee29998
Edward Viaene <ward@in4it.io>
2020-09-07 10:10:05 +0200

    slice for api limit

--- demo-5/securitygroup.tf
+++ demo-5/securitygroup.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "from_europe" {
     from_port   = "443"
     to_port     = "443"
     protocol    = "tcp"
-    cidr_blocks = data.aws_ip_ranges.european_ec2.cidr_blocks
+    cidr_blocks = slice(data.aws_ip_ranges.european_ec2.cidr_blocks, 0, 50)
   }
   tags = {
     CreateDate = data.aws_ip_ranges.european_ec2.create_date
