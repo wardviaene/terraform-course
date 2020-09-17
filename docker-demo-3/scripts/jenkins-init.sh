75fa9b4cd8feb32d809ebcf68607aafc5de74653
Edward Viaene <ward@in4it.io>
2020-09-17 22:11:36 +0200

    move apt-get update before apt-get

--- docker-demo-3/scripts/jenkins-init.sh
+++ docker-demo-3/scripts/jenkins-init.sh
@@ -25,12 +25,12 @@ echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' >> /etc/fstab
 mount /var/lib/jenkins
 
 # install default-jre (needed for ubuntu 18.04)
+apt-get update
 apt-get install -y default-jre
 
 # install jenkins and docker
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
-apt-get update
 apt-get install -y jenkins=${JENKINS_VERSION} unzip docker.io
 
 # enable docker and add perms
