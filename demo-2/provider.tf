663fe4e43b449d04304397b79fb22d142dd7cd60
Saikat <saikat900@hotmail.com>
2021-03-22 21:48:47 +0530

    Customized Demo2

--- demo-2/provider.tf
+++ demo-2/provider.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region     = var.AWS_REGION
+    region     = var.AWS_REGION
 }
 
--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -1,11 +1,11 @@
-variable "AWS_ACCESS_KEY" {
-}
+#variable "AWS_ACCESS_KEY" {
+#}
 
-variable "AWS_SECRET_KEY" {
-}
+#variable "AWS_SECRET_KEY" {
+#}  // Not required if aws is already configured
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-south-1"
 }
 
 variable "AMIS" {
@@ -14,6 +14,7 @@ variable "AMIS" {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
+    ap-south-1 = "ami-03caa8eb4ce0612a2"
   }
 }
 
