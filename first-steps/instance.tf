4713168a97af8ee97ed6eb96fea4c5403f2b23cc
pogopolice <pogopolice@gmail.com>
2021-10-13 13:32:22 -0400

    change to terraform cloud backend

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,28 @@
+terraform {
+  backend "remote" {
+    organization = "example-org-211007"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  profile = "javaapp"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
-  instance_type = "t2.micro"
+  ami           = "ami-036490d46656c4818"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-0a5aaf36bcd8711a3"
 }
 
