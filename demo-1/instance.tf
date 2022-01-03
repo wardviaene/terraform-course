63cb3c882c542ab40b5760c5c077124e34112adb
Edward Viaene <ward@in4it.io>
2022-01-03 14:26:55 -0500

    revert to lookup() to match video

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = var.AMIS[var.AWS_REGION]
+  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
   instance_type = "t2.micro"
 }
 
