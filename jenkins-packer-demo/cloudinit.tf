8ef05a97ea43c617989ad353d8fd47e48fc27873
Edward Viaene <ward@in4it.io>
2022-05-09 14:07:38 -0400

    remove template provider

--- jenkins-packer-demo/cloudinit.tf
+++ jenkins-packer-demo/cloudinit.tf
@@ -1,19 +1,14 @@
-data "template_file" "jenkins-init" {
-  template = file("scripts/jenkins-init.sh")
-  vars = {
-    DEVICE            = var.INSTANCE_DEVICE_NAME
-    JENKINS_VERSION   = var.JENKINS_VERSION
-    TERRAFORM_VERSION = var.TERRAFORM_VERSION
-  }
-}
-
-data "template_cloudinit_config" "cloudinit-jenkins" {
+data "cloudinit_config" "cloudinit-jenkins" {
   gzip          = false
   base64_encode = false
 
   part {
     content_type = "text/x-shellscript"
-    content      = data.template_file.jenkins-init.rendered
+    content      = templatefile("scripts/jenkins-init.sh", {
+      DEVICE            = var.INSTANCE_DEVICE_NAME
+      JENKINS_VERSION   = var.JENKINS_VERSION
+      TERRAFORM_VERSION = var.TERRAFORM_VERSION
+    })
   }
 }
 
--- jenkins-packer-demo/instance.tf
+++ jenkins-packer-demo/instance.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "jenkins-instance" {
   key_name = aws_key_pair.mykeypair.key_name
 
   # user data
-  user_data = data.template_cloudinit_config.cloudinit-jenkins.rendered
+  user_data = data.cloudinit_config.cloudinit-jenkins.rendered
 
   # iam instance profile
   iam_instance_profile = aws_iam_instance_profile.jenkins-role.name
