1fa263e35d34d5f722af3f7368446d5983975cfd
Jacob Sauni <jsauni@gmail.com>
2022-06-15 11:01:19 +1000

    Added stop_instance_before_detaching argument to aws_volume_attachment resource

--- demo-9/instance.tf
+++ demo-9/instance.tf
@@ -22,8 +22,9 @@ resource "aws_ebs_volume" "ebs-volume-1" {
 }
 
 resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.ebs-volume-1.id
-  instance_id = aws_instance.example.id
+  device_name                    = "/dev/xvdh"
+  volume_id                      = aws_ebs_volume.ebs-volume-1.id
+  instance_id                    = aws_instance.example.id
+  stop_instance_before_detaching = true
 }
 
