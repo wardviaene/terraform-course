88988018dfe9dcea0aa8f5328b67cc93f4197b05
Jacob Sauni <jsauni@gmail.com>
2022-07-29 10:38:30 +1000

    Removed patch version from engine_version argument for aws_db_instance.mariadb

--- demo-12/rds.tf
+++ demo-12/rds.tf
@@ -18,7 +18,7 @@ resource "aws_db_parameter_group" "mariadb-parameters" {
 resource "aws_db_instance" "mariadb" {
   allocated_storage       = 100 # 100 GB of storage, gives us more IOPS than a lower number
   engine                  = "mariadb"
-  engine_version          = "10.4.13"
+  engine_version          = "10.4"
   instance_class          = "db.t2.small" # use micro if you want to use the free tier
   identifier              = "mariadb"
   db_name                 = "mariadb"
