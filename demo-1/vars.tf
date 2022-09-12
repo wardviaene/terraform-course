f6b4352b9a98ad96eefc34646db49594d34ee3db
dhanunjayapalla <70584522+dhanunjayapalla@users.noreply.github.com>
2022-09-12 13:29:54 +0530

    Update vars.tf

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,15 +5,13 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-southeast-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    ap-southeast-1 = "ami-02ee763250491e04a"
   }
 }
 
