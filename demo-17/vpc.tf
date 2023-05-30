d05c6c86d529fc1ce2b64354a2100651aaec4bd6
Edward Viaene <ward@in4it.io>
2023-05-30 08:11:51 -0500

    remove enable_classiclink

--- demo-17/vpc.tf
+++ demo-17/vpc.tf
@@ -4,7 +4,6 @@ resource "aws_vpc" "main" {
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
   tags = {
     Name = "main"
   }
