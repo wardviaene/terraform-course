2e54cfa1a85688ddc2457c16501eecc7cee7585c
sivajid788 <119457217+sivajid788@users.noreply.github.com>
2024-08-26 07:48:21 -0700

    Create versions2.tf

---
+++ first-steps/versions2.tf
@@ -0,0 +1,50 @@
+Job Description: Experienced CloudOps Engineer
+Position: CloudOps Engineer
+Experience: 3-5 Years
+Open Positions: 2
+Location: Bangalore
+Employment Type: 4 Months Full Time Contract 
+
+Responsibilities:
+
+Cloud Management:
+
+•	Proficient in managing AWS services, including ECS, IAM role management, and policies.
+•	Ensure optimal performance, scalability, and reliability of cloud infrastructure.
+
+Automation:
+
+•	Automate routine activities and processes using Python.
+•	Develop and maintain automation scripts to streamline operations and improve efficiency.
+
+
+
+Security:
+
+•	Implement and enforce security measures using WAF, NACLs, and Security Groups.
+•	Utilize AWS security tools such as GuardDuty and Security Lake to monitor and protect cloud environments.
+•	Continuously assess and enhance the security posture of cloud resources.
+
+
+
+
+
+Qualifications:
+•	Education: Bachelor's degree in Computer Science, Information Technology, or a related field.
+•	Experience: 3-5 years of hands-on experience in CloudOps, with a strong focus on AWS.   
+•	Certifications in AWS (e.g., AWS Certified Solutions Architect, AWS Certified DevOps Engineer) are a plus.
+•	Strong analytical and problem-solving skills.
+•	Excellent communication and collaboration abilities.
+
+Benefits:
+•	Competitive salary and benefits package.
+•	Opportunities for professional growth and development.
+•	Dynamic and inclusive work environment.
+
+Skills:
+•	Expertise in AWS services, particularly ECS, IAM, and related security practices.
+•	Proficiency in Python for automation and scripting.
+•	Solid understanding of network security principles and tools, including WAF, NACLs, and Security Groups.
+•	Familiarity with AWS security tools like Guard Duty and Security Lake.
+
+
